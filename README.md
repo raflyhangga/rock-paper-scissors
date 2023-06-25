234cd0cf5bf8b289659065a0fb449486e50c478e
Rafly Hanggaraksa <ajarafly422@gmail.com>
2023-06-25 14:15:23 +0700

    Adding algorithms

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors
+
+This is a mini project of mine of a rock paper scissors game using javascript. I make it so that you don't have to access console to played it. Enjoy!
---
+++ script.js
@@ -0,0 +1,102 @@
+function getComputerChoice () {
+    let randNumber = Math.floor(Math.random()*10) + 1;
+    if (randNumber <= 4) {
+        return "ROCK";
+    }
+    else if (randNumber > 4 && randNumber <= 6) {
+        return "PAPER";
+    }
+    else {
+        return "SCISSORS"
+    }
+}
+
+function game(userChoice,enemyChoice) {
+    if (userChoice == "ROCK") {
+        if (enemyChoice == "ROCK") {
+            alert("You Draw!");
+            return "DRAW"
+        }
+        else if (enemyChoice == "PAPER") {
+            alert("You Lose!");
+            return "LOSE";
+        }
+        else {
+            alert("You Win!");
+            return "WIN";
+        }
+    }
+    else if (userChoice == "PAPER") {
+        if (enemyChoice == "ROCK") {
+            alert("You Win!");
+            return "WIN"
+        }
+        else if (enemyChoice == "PAPER") {
+            alert("You Draw!");
+            return "DRAW";
+        }
+        else {
+            alert("You Lose!");
+            return "LOSE";
+        }
+    }
+    else if (userChoice == "SCISSORS") {
+        if (enemyChoice == "ROCK") {
+            alert("You Lose!");
+            return "LOSE"
+        }
+        else if (enemyChoice == "PAPER") {
+            alert("You Win!");
+            return "WIN";
+        }
+        else {
+            alert("You Draw!");
+            return "DRAW";
+        }
+    }
+};
+
+function checkValidity(userChoice) {
+    if (userChoice == "ROCK" || userChoice == "PAPER" || userChoice == "SCISSORS" ) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+let userScore = 0;
+let botScore = 0;
+let totalScore = 0;
+
+
+while (totalScore < 3) {
+    if ((userScore == 2 && botScore == 0) ||(userScore == 0 && botScore == 2)) {
+            break;
+        }
+
+    let userChoice = prompt("Insert your choice! (Rock, Paper, or Scissors?)\nCurrent Score:\nYou: " + userScore + "     Computer: "+ botScore).toUpperCase();
+
+    while (checkValidity(userChoice) == false) {
+        userChoice = prompt("WRONG INPUT!!\nInsert your choice! (Rock, Paper, or Scissors?)\nCurrent Score:\nYou: " + userScore + "     Computer: "+ botScore).toUpperCase();
+    }
+
+
+    let enemyChoice = getComputerChoice();
+    let result = game(userChoice,enemyChoice);
+
+    if (result == "WIN") {
+        ++userScore;
+    }
+    else if (result == "LOSE") {
+        ++botScore;
+    }
+    totalScore = userScore + botScore;
+}
+
+if (userScore > botScore) {
+    alert("You just won the game!")
+}
+else {
+    alert("You lose the game. Thank you for playing!")
+}
